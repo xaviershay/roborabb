d87a422f170988215f9fd266c5cb0e7dc3a0576e
Xavier Shay <hello@xaviershay.com>
2011-12-05 08:12:36 -0800

    Whitespace.

--- spec/unit_spec.rb
+++ spec/unit_spec.rb
@@ -1,7 +1,6 @@
 $LOAD_PATH.unshift(File.expand_path("../../lib", __FILE__))
 require 'roborabb'
 
-
 describe Roborabb2 do
   def notes(rabb)
     rabb.next.notes
@@ -11,33 +10,27 @@ describe Roborabb2 do
     it 'allows a value for notes' do
       rabb = Roborabb2.construct(
         subdivisions: 2,
-        notes: {
-          subdivisions: 'A'
-        }
+        notes:        { a: 'A' }
       )
 
-      notes(rabb).should == {
-        subdivisions: ['A', 'A']
-      }
+      notes(rabb).should == { a: ['A', 'A'] }
     end
 
     it 'includes subdivision in env yielded to notes' do
       rabb = Roborabb2.construct(
         subdivisions: 3,
         notes: {
-          subdivisions: :subdivision.to_proc
+          a: :subdivision.to_proc
         }
       )
 
-      notes(rabb).should == {
-        subdivisions: [0, 1, 2]
-      }
+      notes(rabb).should == { a: [0, 1, 2] }
     end
 
     it 'includes bar number in env yielded to config' do
       rabb = Roborabb2.construct(
         subdivisions: :index.to_proc,
-        notes: { a: 1 }
+        notes:        { a: 1 }
       )
 
       3.times.map { notes(rabb) }.should == [
@@ -50,7 +43,7 @@ describe Roborabb2 do
     it 'includes bar number in env yielded to notes' do
       rabb = Roborabb2.construct(
         subdivisions: 2,
-        notes: { a: L{|e| e.bar.index } }
+        notes:        { a: L{|e| e.bar.index } }
       )
 
       3.times.map { notes(rabb) }.should == [
