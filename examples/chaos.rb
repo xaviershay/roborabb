0a1de0004a4dde0158db798a784cb647c37575f3
Xavier Shay <hello@xaviershay.com>
2011-12-10 12:58:46 -0800

    DRY up chaos example.

--- examples/chaos.rb
+++ examples/chaos.rb
@@ -3,24 +3,29 @@ require 'roborabb'
 
 density = 0.5
 
+SUBDIVISION, UNIT, TIME_SIGNATURE, BEAT_STRUCTURE = *0..3
 templates = [
   [7, 16, "7/16", [3, 2, 2]],
-  [5, 8, "5/8", [3, 2]],
-  [6, 8, "3/4"],
+  [5, 8,  "5/8",  [3, 2]],
+  [6, 8,  "3/4",  nil],
 ]
 
-bars = 16.times.map { templates.sample }
+bars        = 16.times.map { templates.sample }
+random_note = L{|env| rand >= 1 - density }
+random_bar  = L{|index|
+  L{|bar| bars[bar.index % bars.length][index] }
+}
 
 generator = Roborabb.construct(
   title:          "Chaos",
-  subdivisions:   L{|bar| bars[bar.index % bars.length][0] },
-  unit:           L{|bar| bars[bar.index % bars.length][1] },
-  time_signature: L{|bar| bars[bar.index % bars.length][2] },
-  beat_structure: L{|bar| bars[bar.index % bars.length][3] },
+  subdivisions:   random_bar[SUBDIVISION],
+  unit:           random_bar[UNIT],
+  time_signature: random_bar[TIME_SIGNATURE],
+  beat_structure: random_bar[BEAT_STRUCTURE],
   notes: {
-    hihat: L{|env| rand >= 1 - density },
-    kick:  L{|env| rand >= 1 - density},
-    snare: L{|env| rand >= 1 - density }
+    hihat: random_note,
+    kick:  random_note,
+    snare: random_note
   }
 )
 
